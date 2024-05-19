458b92c6494aedf8bb7c4ba10071f3a15bed1594
bradley.leopold <bradley.leopold@gmail.com>
2024-05-19 14:09:17 -0400

    Initial commit from template project.

---
+++ InventoryApp.Client/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+  }
+
+  title = 'inventoryapp.client';
+}
